ce7821873dd6216ce8252ca398662e03237304b5
ste-ioan <72309874+ste-ioan@users.noreply.github.com>
2021-04-07 16:13:50 +0200

    Add files via upload

--- analysis/questionnairestestnplots.m
+++ analysis/questionnairestestnplots.m
@@ -6,40 +6,6 @@
 % results = readtable('results - questionnaires.csv');
 results = readtable('~/Downloads/results - questionnaires.csv');
 
-% i will put the tests in a coherent way. all answers are 1 yes 5 no
-% take abs so that positive change = more tired
-
-% item 1 'i feel very in shape' 
-DeltaQuestio(:,1) = abs(results.MFI1_Pre - results.MFI1_Post); 
-% item 2 'physically i don't feel like doing much' 
-DeltaQuestio(:,2) = results.MFI2_Pre - results.MFI2_Post;
-% item 3 'i feel very active'
-DeltaQuestio(:,3) = abs(results.MFI3_Pre - results.MFI3_Post);
-% item 4 'i feel tired'
-DeltaQuestio(:,4) = results.MFI4_Pre - results.MFI4_Post;
-% item 5 'i can concentrate generally'
-DeltaQuestio(:,5) = abs(results.MFI5_Pre - results.MFI5_Post);
-% item 6 'i can do alot physically)
-DeltaQuestio(:,6) = abs(results.MFI6_Pre - results.MFI6_Post);
-% item 7 'i'd like to avoid doing things'
-DeltaQuestio(:,7) = results.MFI7_Pre - results.MFI7_Post;
-% item 8 'i can concentrate now'
-DeltaQuestio(:,8) = abs(results.MFI8_Pre - results.MFI8_Post);
-% item 9 'i feel rested'
-DeltaQuestio(:,9) = abs(results.MFI9_Pre - results.MFI9_Post);
-% item 10 'i struggle with concentrating on something'
-DeltaQuestio(:,10) = results.MFI10_Pre - results.MFI10_Post;
-% item 11 'i feel physically bad'
-DeltaQuestio(:,11) = results.MFI11_Pre - results.MFI11_Post;
-% item 12 'i fatigue easily'
-DeltaQuestio(:,12) = results.MFI12_Pre - results.MFI12_Post;
-% item 13 'i don't feel like doing anything'
-DeltaQuestio(:,13) = results.MFI13_Pre - results.MFI13_Post; 
-% item 14 'my thoughts cross easily'
-DeltaQuestio(:,14) = results.MFI14_Pre - results.MFI14_Post;
-% item 15 'i feel perfect physically'
-DeltaQuestio(:,15) = abs(results.MFI15_Pre - results.MFI15_Post);
-
 % karolinska scale, higher number = more sleepiness
 Karopre = results.KarolinskaPre;
 Karopost= results.KarolinskaPost;
@@ -56,12 +22,12 @@ negative_items = [2, 4, 7, 10, 11, 12, 13, 14];
 
 for k = 1:numel(negative_items)    
    tmp_pre(:,k) = eval(['results.MFI',num2str(negative_items(k)),'_Pre']);
-    tmp_post(:,k) = eval(['results.MFI',num2str(negative_items(k)),'_Pre']);
+    tmp_post(:,k) = eval(['results.MFI',num2str(negative_items(k)),'_Post']);
 end
 
 for jj = 1:length(tmp_pre)
 
-for ll = 1:(negative_items)
+for ll = 1:numel(negative_items)
     switch tmp_pre(jj, ll)       
         case 5
           tmp_pre(jj, ll) = 1;
@@ -77,7 +43,7 @@ end
 
 for jj = 1:length(tmp_post)
 
-for ll = 1:(negative_items)
+for ll = 1:numel(negative_items)
     switch tmp_post(jj, ll)       
         case 5
           tmp_post(jj, ll) = 1;
@@ -91,11 +57,15 @@ for ll = 1:(negative_items)
 end
 end
 
-MFI_pre = [results.MFI1_Pre,results.MFI3_Pre,results.MFI5_Pre,results.MFI6_Pre,...
-    results.MFI8_Pre,results.MFI9_Pre,results.MFI15_Pre, tmp_pre];
+MFI_pre = [results.MFI1_Pre,tmp_pre(:,1),results.MFI3_Pre,tmp_pre(:,2),...
+    results.MFI5_Pre, results.MFI6_Pre, tmp_pre(:,3), results.MFI8_Pre,...
+    results.MFI9_Pre, tmp_pre(:,4), tmp_pre(:,5), tmp_pre(:,6),tmp_pre(:,7),...
+    tmp_pre(:,8), results.MFI15_Pre];
 
-MFI_post = [results.MFI1_Post,results.MFI3_Post,results.MFI5_Post,results.MFI6_Post,...
-    results.MFI8_Post,results.MFI9_Post,results.MFI15_Post, tmp_post];
+MFI_post = [results.MFI1_Post,tmp_post(:,1),results.MFI3_Post,tmp_post(:,2),...
+    results.MFI5_Post, results.MFI6_Post, tmp_post(:,3), results.MFI8_Post,...
+    results.MFI9_Post, tmp_post(:,4), tmp_post(:,5), tmp_post(:,6),tmp_post(:,7),...
+    tmp_post(:,8), results.MFI15_Post];
 
 T_MFI = array2table([mean(MFI_pre,2), mean(MFI_post,2)]);
 T_MFI (:,end+1) = cell2table(repmat({'easy','hard'}', 24,1));
@@ -110,22 +80,21 @@ Behaviour = readtable('~/ownCloud/MATLAB/Scripts/TDT/original/private/analysis/d
 
 DeltaSaturationBeg = Behaviour.SATbeg- Behaviour.NONSATbeg;
 DeltaSaturationFin = Behaviour.SATfin - Behaviour.NONSATfin;
-DeltaofDeltaBehav = DeltaSaturationFin - DeltaSaturationBeg;
+DeltaBehavior = DeltaSaturationFin - DeltaSaturationBeg;
 
-
-deltaMFI = mean(DeltaQuestio,2);
+deltaMFI = mean(MFI_post-MFI_pre,2);
 color = rgb('dark green');
-plot(DeltaofDeltaBehav,deltaMFI,'Color', color, 'Marker', '.', 'MarkerSize', 30, 'LineStyle', 'none')
-[rhomfi, corrpvalmfi] = corr(deltaMFI,DeltaofDeltaBehav);
+plot(DeltaBehavior,deltaMFI,'Color', color, 'Marker', '.', 'MarkerSize', 30, 'LineStyle', 'none')
+[rhomfi, corrpvalmfi] = corr(deltaMFI,DeltaBehavior);
 
 mycorr = @(deltaMFI,DeltaofDeltaBehav) corr(deltaMFI,DeltaofDeltaBehav);
 nIterations = 10000;
-corr_ci = bootci(nIterations,{mycorr,deltaMFI,DeltaofDeltaBehav});
+corr_ci = bootci(nIterations,{mycorr,deltaMFI,DeltaBehavior});
 
 % for the figure and data sharingg
-% data = table(mean(DeltaQuestio,2),DeltaofDelta);
+% data = table(deltaMFI,DeltaBehavior);
 % data.Properties.VariableNames = {'MFI', 'Behaviour'};
 % writetable(data, '~/ownCloud/MATLAB/Scripts/TDT/original/private/analysis/data_extracted/Tables/Behavior/QuestioCorrel.txt')
 
 % do corr on sleepiness scores
-[rhoKaro, corrpvalKaro] = corr(deltaKarolinska,DeltaofDeltaBehav, 'type', 'Spearman');
+[rhoKaro, corrpvalKaro] = corr(deltaKarolinska,DeltaBehavior, 'type', 'Spearman');
