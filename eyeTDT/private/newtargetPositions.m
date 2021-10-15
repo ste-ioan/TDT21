20dec48d96ed405c85828f6f71376626b1b20583
ste-ioan <72309874+ste-ioan@users.noreply.github.com>
2021-10-15 23:58:13 +0200

    Update newtargetPositions.m

--- eyeTDT/private/newtargetPositions.m
+++ eyeTDT/private/newtargetPositions.m
@@ -12,9 +12,6 @@ rng('shuffle')
 Q1 = 289; % position code for quadrant 1 target
 Q2 = 61; % position code for quadrant 2 target
 
-% we'll also add target alignment info here to have an equal number per
-% condition and target, which we will then shuffle
-
 % position in grid matrix
 targetpos(1,:) = [repmat(Q1, 1, ntrials/2),repmat(Q2, 1, ntrials/2)]; 
 % target alignment, horizontal or vertical
@@ -25,7 +22,6 @@ targetpos =Shuffle(targetpos,1);
 if ITI
 temp = nan(1,ntrials);
 
- % without orientation here
 for b = 1:2
 for k = [Q1 Q2]
 temp(targetpos(1,:) == k & targetpos(2,:) == b) = Shuffle(repmat([.2 .4 .6 .8], 1, (ntrials/4)/4)); %ntrials divided by number of conditions, divided by number of ITIs     
